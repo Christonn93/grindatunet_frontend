ae9514e4532d66ae97ee93535ceb7fdcf17fd3d0
Christopher Tonnesland <christopher.tonnesland@redgo.no>
2025-03-12 13:49:04 +0100

    adding new content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/form/LoginForm";
 
 export const Home = () => {
  return (
-  <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+  <div className="flex flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
    <div className="w-full max-w-sm">
     <LoginForm />
    </div>
